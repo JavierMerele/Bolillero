8cca2faf1aea70c38fac987f8a1c1351b7df0697
JavierMerele <javiermereleet12d1@gmail.com>
2023-07-10 17:07:52 -0300

    async

--- Bolilleros/Simulacion.cs
+++ Bolilleros/Simulacion.cs
@@ -13,6 +13,13 @@ public class Simulacion
 
     public long simularConHilos(Bolillero original, List<int> jugadas, int cantidadSimulaciones, int cantidadHilos)
     {
+        var hilos = GenerarProcesos(original, jugadas, cantidadSimulaciones, cantidadHilos);
+        Task<long>.WaitAll(hilos);
+        return hilos.Sum(s => s.Result);
+    }
+
+    private static Task<long>[] GenerarProcesos(Bolillero original, List<int> jugadas, int cantidadSimulaciones, int cantidadHilos)
+    {
         Task<long>[] hilos = new Task<long>[cantidadHilos];
         var tarea = cantidadSimulaciones / cantidadHilos;
 
@@ -21,8 +28,15 @@ public class Simulacion
             var bolillero = original.clonar();
             hilos[i] = Task<long>.Run(() => (long)bolillero.JugarNveces(jugadas, tarea));
         }
-        Task<long>.WaitAll(hilos);
-        return hilos.Sum(s => s.Result);
+
+        return hilos;
     }
 
+    public async Task<long> simularConHilosAsync(Bolillero original, List<int> jugadas, int cantidadSimulaciones, int cantidadHilos)
+    {
+        var hilos = GenerarProcesos(original, jugadas, cantidadSimulaciones, cantidadHilos);
+        await Task<long>.WhenAll(hilos);
+        return hilos.Sum(s => s.Result);
+
+    }
 }
